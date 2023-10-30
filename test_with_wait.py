62b821f2e0592faf66cdf1a5d1fc5710bec4e7e0
Katie Lovell <katielovell@Katies-MacBook-Air.local>
2023-10-30 20:42:45 +0000

    with wait until page load

--- test_with_wait.py
+++ test_with_wait.py
@@ -1,10 +1,8 @@
 from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 import time
 import random
-from bs4 import BeautifulSoup 
+from bs4 import BeautifulSoup
+from selenium.webdriver.support.ui import WebDriverWait 
 
 # real url - might need replacing
 # url = 'https://glastonbury.seetickets.com/content/extras'
@@ -27,18 +25,21 @@ def gen_page ():
     return str(page_choice)
 
 def refresh_page ():
-        time.sleep(10)
         driver.get(gen_page())
-        # will need to add back in the refresh
         # driver.refresh()
+        time.sleep(1)
 
 while is_queue_page == True: # this will change to the while false or something 
+    
+    try:
+         WebDriverWait()
+         
     refresh_page()
-    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
     page_html = str(BeautifulSoup(driver.page_source, 'html.parser'))
     # if postcode appears on queue page this will not work. 
     # If postcode does not appear on main page this will not work
     if page_html.__contains__("postcode") or page_html.__contains__("Postcode"):
+        print("banging")
         is_queue_page = False
 
 
