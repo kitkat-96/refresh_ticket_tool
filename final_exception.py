45a331ee01261498ce9a8dddda1b3a4edaf5e6be
Katie Lovell <katielovell@Katies-MacBook-Air.local>
2023-10-31 21:38:40 +0000

    few different options

--- final_exception.py
+++ final_exception.py
@@ -21,12 +21,19 @@ while is_queue_page == True:
     time.sleep(1)
     
     try:
+        # can change the wait time if this is too long
         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
         page_html = str(BeautifulSoup(driver.page_source, 'html.parser'))
+        # if it doesn't stop then maybe postcode issue
         if page_html.__contains__("postcode") or page_html.__contains__("Postcode"):
             is_queue_page = False
 
+    # can change exception type?
     except TimeoutException:
-        print("Not loaded in time - Refresh")   
+        print("Not loaded in time - Refresh")
+
+    # to just Exception? do we want both?
+    except Exception:
+        print("Not loaded in time - Refresh")     
      
 
--- final_no_wdw.py
+++ final_no_wdw.py
@@ -1,7 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 import time
 from bs4 import BeautifulSoup 
 
@@ -18,9 +15,8 @@ while is_queue_page == True:
     
     driver.refresh()
     time.sleep(1)
-    # can change the wait time if this is too long
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
-    # see if it waits too long with the exception 
+
+# did we need a web driver wait
     page_html = str(BeautifulSoup(driver.page_source, 'html.parser'))
     # if it doesn't stop then maybe postcode issue
     if page_html.__contains__("postcode") or page_html.__contains__("Postcode"):
