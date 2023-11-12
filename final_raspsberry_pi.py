3c00f6c398089eb2b84779877a57d083812b437e
Katie Lovell <katielovell@Katies-MacBook-Air.local>
2023-11-12 16:23:50 +0000

    update

--- final_raspsberry_pi.py
+++ final_raspsberry_pi.py
@@ -6,33 +6,26 @@ from selenium.common.exceptions import TimeoutException
 import time
 from bs4 import BeautifulSoup 
 
-# real url - might need replacing
+# If glasto URL is different, replace this
 glasto_url = 'https://glastonbury.seetickets.com/content/extras'
 is_queue_page = True
-# will want to set headers 
-headers = {}
 
-driver = webdriver.Firefox()
+driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver')
 driver.get(glasto_url)
 
 while is_queue_page == True:
-    
     driver.refresh()
     time.sleep(1)
-    
     try:
-        # can change the wait time if this is too long. 
         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
         page_html = str(BeautifulSoup(driver.page_source, 'html.parser'))
-        # if it doesn't stop then maybe postcode issue
+        
         if page_html.__contains__("postcode") or page_html.__contains__("Postcode"):
             is_queue_page = False
 
-    # can change exception type?
     except TimeoutException:
         print("Not loaded in time - Refresh")
 
-    # to just Exception? do we want both?
     except Exception:
         print("Not loaded in time - Refresh")     
      
