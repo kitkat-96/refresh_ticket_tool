1928d7762cec48b7d692aca41ad6267f4df4786d
Katie Lovell <katielovell@Katies-MacBook-Air.local>
2023-11-16 17:11:21 +0000

    read deposit

--- final_firefox.py
+++ final_firefox.py
@@ -27,7 +27,7 @@ while is_queue_page == True:
         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
         page_html = str(BeautifulSoup(driver.page_source, 'html.parser'))
         # if it doesn't stop then maybe postcode is issue
-        if "postcode" in page_html.lower():
+        if "deposit" in page_html.lower():
             is_queue_page = False
 
     except TimeoutException:
